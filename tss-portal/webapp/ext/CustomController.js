8c1729e9ca5c3048fdbec406cbf5f8bb00d8ec12
Deepanjali Kumari <Deepanjali.Kumari@extentia.com>
2022-02-21 15:05:51 +0000

    new changes

--- tss-portal/webapp/ext/CustomController.js
+++ tss-portal/webapp/ext/CustomController.js
@@ -6,9 +6,9 @@ sap.ui.define([
     return {
 
         onDownloadPress: function (oEvent) {
-           debugger;
+            debugger;
             var downloadAttachmentLink = `${this.getInterface().getModel("fileService").sServiceUrl}DownloadDefectCSVSampleFiles(ID=e8a1f0d0-e157-4d4b-8bcf-27ca5e192336)/content`
-            sap.m.URLHelper.redirect(downloadAttachmentLink, true);  
+            sap.m.URLHelper.redirect(downloadAttachmentLink, true);
 
         },
         onUploadPress: function (oEvent) {
@@ -17,8 +17,19 @@ sap.ui.define([
                 //create instance of fragment
                 this.importDialog = sap.ui.xmlfragment("com.knpl.tssportal.ext.fragment.FileUploadImport", this);
             }
-            this.getInterface().addDependent(this.importDialog);
+            // this.getInterface().addDependent(this.importDialog);
             this.importDialog.open();
+
+
+            // this.importDialog.attachAfterOpen(function () {
+
+            //     sap.ui.getCore().byId("btnUpload").attachBrowserEvent("click", this.handleUploadPress);
+
+            // }.bind(this));
+        },
+
+        handleUploadPress: function (oEvent) {
+            debugger;
         },
 
         handleCancelPress: function (oEvent) {
---
+++ tss-portal/webapp/ext/controller/DefectDetailsList.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+   
+    
+], function (controller) {
+    "use strict";
+
+    return controller.extend("com.knpl.tssportal.ext.controller.DefectDetailsList", {
+
+        onInit: function () {
+ },
+              handleUploadPress: function (oEvent) {
+           debugger;
+     
+        },
+    });
+});
